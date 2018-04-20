67b76914e6cafea9df71ab998c90ad0695c73bc7
NONstope <khodienkod@gmail.com>
2018-04-20 22:55:31 +0300

    db configs added.

---
+++ configs/config-db.php
@@ -0,0 +1,11 @@
+<?php
+
+$dbhost = "localhost";
+$dbname = "sphere";
+$dbuser = "root";
+
+$conn = mysqli_connect($dbhost, $dbuser, "", $dbname)
+	or die("Error while connectiong");
+mysqli_query($conn, "SET character_set_results = 'utf8'");
+
+?>
